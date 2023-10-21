bc04312f93cb778443ae49154ca63ae8e6c5fcfe
Kohei Yamaguchi <fix7211@gmail.com>
2023-10-21 23:04:43 +0900

    Add np-opt tool

---
+++ tools/np-opt/np-opt.cpp
@@ -0,0 +1,29 @@
+//===- np-opt.cpp - The numpy compiler-----------------------------------===//
+//
+// This file executes as entry point of numpy compiler.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/DialectRegistry.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Pass/PassManager.h"
+#include "llvm/Support/CommandLine.h"
+
+#include "InitAll.h"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+  mlir::npc::registerAllPasses();
+
+  DialectRegistry registry;
+  registry.insert<mlir::func::FuncDialect>();
+  registry.insert<mlir::LLVM::LLVMDialect>();
+  mlir::npc::registerAllDialects(registry);
+
+  return asMainReturnCode(MlirOptMain(argc, argv, "Numpy Compiler Opt Tool", registry));
+}
