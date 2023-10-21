037784e6d2ce62eecfaf8ec62d4bc963a7870641
Kohei Yamaguchi <fix7211@gmail.com>
2023-10-21 21:37:12 +0900

    Add other basicpy's operation control

--- lib/Dialect/Basicpy/IR/BasicpyDialect.cpp
+++ lib/Dialect/Basicpy/IR/BasicpyDialect.cpp
@@ -25,34 +25,34 @@ Type BasicpyDialect::parseType(DialectAsmParser &parser) const {
 
   if (keyword == "BoolType")
     return BoolType::get(getContext());
-  //if (keyword == "BytesType")
-  //  return BytesType::get(getContext());
-  //if (keyword == "EllipsisType")
-  //  return EllipsisType::get(getContext());
-  //if (keyword == "NoneType")
-  //  return NoneType::get(getContext());
-  //if (keyword == "SlotObject") {
-  //  StringRef className;
-  //  if (parser.parseLess() || parser.parseKeyword(&className)) {
-  //    return Type();
-  //  }
-  //  llvm::SmallVector<Type, 4> slotTypes;
-  //  while (succeeded(parser.parseOptionalComma())) {
-  //    Type slotType;
-  //    if (parser.parseType(slotType))
-  //      return Type();
-  //    slotTypes.push_back(slotType);
-  //  }
-  //  if (parser.parseGreater())
-  //    return Type();
+  if (keyword == "BytesType")
+    return BytesType::get(getContext());
+  if (keyword == "EllipsisType")
+    return EllipsisType::get(getContext());
+  if (keyword == "NoneType")
+    return NoneType::get(getContext());
+  if (keyword == "SlotObject") {
+    StringRef className;
+    if (parser.parseLess() || parser.parseKeyword(&className)) {
+      return Type();
+    }
+    llvm::SmallVector<Type, 4> slotTypes;
+    while (succeeded(parser.parseOptionalComma())) {
+      Type slotType;
+      if (parser.parseType(slotType))
+        return Type();
+      slotTypes.push_back(slotType);
+    }
+    if (parser.parseGreater())
+      return Type();
 
-  //  return SlotObjectType::get(StringAttr::get(getContext(), className),
-  //                             slotTypes);
-  //}
-  //if (keyword == "StrType")
-  //  return StrType::get(getContext());
-  //if (keyword == "UnknownType")
-  //  return UnknownType::get(getContext());
+    return SlotObjectType::get(StringAttr::get(getContext(), className),
+                               slotTypes);
+  }
+  if (keyword == "StrType")
+    return StrType::get(getContext());
+  if (keyword == "UnknownType")
+    return UnknownType::get(getContext());
 
   parser.emitError(parser.getNameLoc(), "unknown type in 'basicpy' dialect: ")
       << keyword;
@@ -62,22 +62,22 @@ Type BasicpyDialect::parseType(DialectAsmParser &parser) const {
 void BasicpyDialect::printType(Type type, DialectAsmPrinter &os) const {
   TypeSwitch<Type>(type)
       .Case<Basicpy::BoolType>([&](Type) { os << "BoolType"; })
-      //.Case<Basicpy::BytesType>([&](Type) { os << "BytesType"; })
-      //.Case<Basicpy::EllipsisType>([&](Type) { os << "EllipsisType"; })
-      //.Case<Basicpy::NoneType>([&](Type) { os << "NoneType"; })
-      //.Case<Basicpy::SlotObjectType>([&](Type) {
-      //  auto slotObject = type.cast<SlotObjectType>();
-      //  auto slotTypes = slotObject.getSlotTypes();
-      //  os << "SlotObject<" << slotObject.getClassName().getValue();
-      //  if (!slotTypes.empty()) {
-      //    os << ", ";
-      //    llvm::interleaveComma(slotTypes, os,
-      //                          [&](Type t) { os.printType(t); });
-      //  }
-      //  os << ">";
-      //})
-      //.Case<Basicpy::StrType>([&](Type) { os << "StrType"; })
-      //.Case<Basicpy::UnknownType>([&](Type) { os << "UnknownType"; })
+      .Case<Basicpy::BytesType>([&](Type) { os << "BytesType"; })
+      .Case<Basicpy::EllipsisType>([&](Type) { os << "EllipsisType"; })
+      .Case<Basicpy::NoneType>([&](Type) { os << "NoneType"; })
+      .Case<Basicpy::SlotObjectType>([&](Type) {
+        auto slotObject = type.cast<SlotObjectType>();
+        auto slotTypes = slotObject.getSlotTypes();
+        os << "SlotObject<" << slotObject.getClassName().getValue();
+        if (!slotTypes.empty()) {
+          os << ", ";
+          llvm::interleaveComma(slotTypes, os,
+                                [&](Type t) { os.printType(t); });
+        }
+        os << ">";
+      })
+      .Case<Basicpy::StrType>([&](Type) { os << "StrType"; })
+      .Case<Basicpy::UnknownType>([&](Type) { os << "UnknownType"; })
       .Default(
           [](Type) { llvm_unreachable("Unexpected Basicpy's type kind"); });
 }
@@ -86,49 +86,49 @@ void BasicpyDialect::printType(Type type, DialectAsmPrinter &os) const {
 // Type and attribute detail
 //----------------------------------------------------------------------------//
 
-//namespace mlir {
-//namespace npc {
-//namespace Basicpy {
-//namespace detail {
-//
-//struct SlotObjectTypeStorage : public TypeStorage {
-//  using KeyTy = std::pair<StringAttr, ArrayRef<Type>>;
-//  SlotObjectTypeStorage(StringAttr className, ArrayRef<Type> slotTypes)
-//      : className(className), slotTypes(slotTypes) {}
-//
-//  bool operator==(const KeyTy &other) const {
-//    return className == other.first && slotTypes == other.second;
-//  }
-//
-//  static llvm::hash_code hashKey(const KeyTy &key) {
-//    return llvm::hash_combine(key.first, key.second);
-//  }
-//
-//  static SlotObjectTypeStorage *construct(TypeStorageAllocator &allocator,
-//                                          const KeyTy &key) {
-//    ArrayRef<Type> slotTypes = allocator.copyInto(key.second);
-//    return new (allocator.allocate<SlotObjectTypeStorage>())
-//        SlotObjectTypeStorage(key.first, slotTypes);
-//  }
-//
-//  StringAttr className;
-//  ArrayRef<Type> slotTypes;
-//};
-//
-//} // namespace detail
-//} // namespace Basicpy
-//} // namespace il
-//} // namespace mlir
-//
-//StringAttr SlotObjectType::getClassName() { return getImpl()->className; }
-//ArrayRef<Type> SlotObjectType::getSlotTypes() { return getImpl()->slotTypes; }
-//unsigned SlotObjectType::getSlotCount() { return getImpl()->slotTypes.size(); }
-//
-//SlotObjectType SlotObjectType::get(StringAttr className,
-//                                   ArrayRef<Type> slotTypes) {
-//  return Base::get(className.getContext(), className, slotTypes);
-//}
-//
+namespace mlir {
+namespace npc {
+namespace Basicpy {
+namespace detail {
+
+struct SlotObjectTypeStorage : public TypeStorage {
+  using KeyTy = std::pair<StringAttr, ArrayRef<Type>>;
+  SlotObjectTypeStorage(StringAttr className, ArrayRef<Type> slotTypes)
+      : className(className), slotTypes(slotTypes) {}
+
+  bool operator==(const KeyTy &other) const {
+    return className == other.first && slotTypes == other.second;
+  }
+
+  static llvm::hash_code hashKey(const KeyTy &key) {
+    return llvm::hash_combine(key.first, key.second);
+  }
+
+  static SlotObjectTypeStorage *construct(TypeStorageAllocator &allocator,
+                                          const KeyTy &key) {
+    ArrayRef<Type> slotTypes = allocator.copyInto(key.second);
+    return new (allocator.allocate<SlotObjectTypeStorage>())
+        SlotObjectTypeStorage(key.first, slotTypes);
+  }
+
+  StringAttr className;
+  ArrayRef<Type> slotTypes;
+};
+
+} // namespace detail
+} // namespace Basicpy
+} // namespace npc
+} // namespace mlir
+
+StringAttr SlotObjectType::getClassName() { return getImpl()->className; }
+ArrayRef<Type> SlotObjectType::getSlotTypes() { return getImpl()->slotTypes; }
+unsigned SlotObjectType::getSlotCount() { return getImpl()->slotTypes.size(); }
+
+SlotObjectType SlotObjectType::get(StringAttr className,
+                                   ArrayRef<Type> slotTypes) {
+  return Base::get(className.getContext(), className, slotTypes);
+}
+
 ////----------------------------------------------------------------------------//
 //// CPA Interface Implementation
 ////----------------------------------------------------------------------------//
@@ -143,9 +143,8 @@ void BasicpyDialect::initialize() {
 #define GET_OP_LIST
 #include "Dialect/Basicpy/IR/BasicpyOps.cpp.inc"
       >();
-  //addTypes<BoolType, BytesType, EllipsisType, NoneType, SlotObjectType, StrType,
-  //         UnknownType>();
-  addTypes<BoolType>();
+  addTypes<BoolType, BytesType, EllipsisType, NoneType, SlotObjectType, StrType,
+           UnknownType>();
 
   // TODO: Make real ops for everything we need.
   allowUnknownOperations();
