1e998265a5c4d9192879a7aebe99f82f8f848069
Kohei Yamaguchi <fix7211@gmail.com>
2023-10-22 23:44:13 +0900

    Add numpy inference

--- lib/Dialect/Numpy/IR/NumpyOps.cpp
+++ lib/Dialect/Numpy/IR/NumpyOps.cpp
@@ -20,66 +20,66 @@ using namespace mlir::npc::Numpy;
 /// Adds constraints to relating a unary op that accepts and returns either
 /// tensor or ndarray types where the dtype should be the same.
 /// Type constraints are added on the dtype, not the outer object type.
-//static void constraintUnaryDtypeInvariantOp(Typing::CPA::Context &context,
-//                                            Value source, Value dest,
-//                                            Operation *op) {
-//  auto &env = context.getCurrentEnvironment();
-//  auto *sourceTn =
-//      llvm::dyn_cast<Typing::CPA::ObjectValueType>(env.mapValueToType(source));
-//  auto *destTn =
-//      llvm::dyn_cast<Typing::CPA::ObjectValueType>(env.mapValueToType(dest));
-//  if (sourceTn && destTn && sourceTn->getFieldCount() == 1 &&
-//      destTn->getFieldCount() == 1) {
-//    context.getConstraint(sourceTn->getFieldTypes().front(),
-//                          destTn->getFieldTypes().front());
-//  }
-//}
-//
-//void CreateArrayFromTensorOp::addCPAConstraints(Typing::CPA::Context &context) {
-//  constraintUnaryDtypeInvariantOp(context, getSource(), getDest(), *this);
-//}
-//
-//void CopyToTensorOp::addCPAConstraints(Typing::CPA::Context &context) {
-//  constraintUnaryDtypeInvariantOp(context, getSource(), getDest(), *this);
-//}
-//
-//void BuiltinUfuncCallOp::addCPAConstraints(Typing::CPA::Context &context) {
-//  // TODO: This should really be a function call chosen so as to promote
-//  // arguments. For now, though, we just say that the result is constrained
-//  // to the inputs. Note that not all ufuncs transfer types like this.
-//  // We just pretend this is two unary functions that write into the output.
-//  for (auto input : getInputs()) {
-//    constraintUnaryDtypeInvariantOp(context, input, getOutput(), *this);
-//  }
-//}
-//
-////===----------------------------------------------------------------------===//
-//// CreateArrayFromTensorOp
-////===----------------------------------------------------------------------===//
-//
-//namespace {
-///// Match create_array_from_tensor -> copy_to_tensor and elide in favor
-///// of the original tensor.
-//class ElideCreateRedundantArrayFromTensor
-//    : public OpRewritePattern<CopyToTensorOp> {
-//public:
-//  using OpRewritePattern::OpRewritePattern;
-//  LogicalResult matchAndRewrite(CopyToTensorOp op,
-//                                PatternRewriter &rewriter) const override {
-//    auto createArrayOp = dyn_cast_or_null<CreateArrayFromTensorOp>(
-//        op.getSource().getDefiningOp());
-//    if (createArrayOp && createArrayOp.getDest().hasOneUse()) {
-//      rewriter.replaceOp(op, createArrayOp.getSource());
-//    }
-//    return success();
-//  }
-//};
-//} // namespace
-//
-//void CopyToTensorOp::getCanonicalizationPatterns(RewritePatternSet &results,
-//                                                 MLIRContext *context) {
-//  results.insert<ElideCreateRedundantArrayFromTensor>(context);
-//}
+static void constraintUnaryDtypeInvariantOp(Typing::CPA::Context &context,
+                                            Value source, Value dest,
+                                            Operation *op) {
+  auto &env = context.getCurrentEnvironment();
+  auto *sourceTn =
+      llvm::dyn_cast<Typing::CPA::ObjectValueType>(env.mapValueToType(source));
+  auto *destTn =
+      llvm::dyn_cast<Typing::CPA::ObjectValueType>(env.mapValueToType(dest));
+  if (sourceTn && destTn && sourceTn->getFieldCount() == 1 &&
+      destTn->getFieldCount() == 1) {
+    context.getConstraint(sourceTn->getFieldTypes().front(),
+                          destTn->getFieldTypes().front());
+  }
+}
+
+void CreateArrayFromTensorOp::addCPAConstraints(Typing::CPA::Context &context) {
+  constraintUnaryDtypeInvariantOp(context, getSource(), getDest(), *this);
+}
+
+void CopyToTensorOp::addCPAConstraints(Typing::CPA::Context &context) {
+  constraintUnaryDtypeInvariantOp(context, getSource(), getDest(), *this);
+}
+
+void BuiltinUfuncCallOp::addCPAConstraints(Typing::CPA::Context &context) {
+  // TODO: This should really be a function call chosen so as to promote
+  // arguments. For now, though, we just say that the result is constrained
+  // to the inputs. Note that not all ufuncs transfer types like this.
+  // We just pretend this is two unary functions that write into the output.
+  for (auto input : getInputs()) {
+    constraintUnaryDtypeInvariantOp(context, input, getOutput(), *this);
+  }
+}
+
+//===----------------------------------------------------------------------===//
+// CreateArrayFromTensorOp
+//===----------------------------------------------------------------------===//
+
+namespace {
+/// Match create_array_from_tensor -> copy_to_tensor and elide in favor
+/// of the original tensor.
+class ElideCreateRedundantArrayFromTensor
+    : public OpRewritePattern<CopyToTensorOp> {
+public:
+  using OpRewritePattern::OpRewritePattern;
+  LogicalResult matchAndRewrite(CopyToTensorOp op,
+                                PatternRewriter &rewriter) const override {
+    auto createArrayOp = dyn_cast_or_null<CreateArrayFromTensorOp>(
+        op.getSource().getDefiningOp());
+    if (createArrayOp && createArrayOp.getDest().hasOneUse()) {
+      rewriter.replaceOp(op, createArrayOp.getSource());
+    }
+    return success();
+  }
+};
+} // namespace
+
+void CopyToTensorOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                                 MLIRContext *context) {
+  results.insert<ElideCreateRedundantArrayFromTensor>(context);
+}
 
 #define GET_OP_CLASSES
 #include "Dialect/Numpy/IR/NumpyOps.cpp.inc"
