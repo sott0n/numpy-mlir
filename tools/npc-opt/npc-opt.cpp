ce90886cd1f3f2307881f67893a2410ddd0cbb49
Kohei Yamaguchi <fix7211@gmail.com>
2023-10-26 00:22:37 +0900

    Rename np-opt -> npc-opt

---
+++ tools/npc-opt/npc-opt.cpp
@@ -0,0 +1,33 @@
+//===- np-opt.cpp - The numpy compiler-----------------------------------===//
+//
+// This file executes as entry point of numpy compiler.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/DialectRegistry.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/Arith/IR/Arith.h"
+#include "mlir/Dialect/SCF/IR/SCF.h"
+#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/Pass/PassManager.h"
+#include "llvm/Support/CommandLine.h"
+
+#include "InitAll.h"
+
+using namespace mlir;
+
+int main(int argc, char **argv) {
+  mlir::npc::registerAllPasses();
+
+  DialectRegistry registry;
+  registry.insert<mlir::func::FuncDialect>();
+  registry.insert<mlir::arith::ArithDialect>();
+  registry.insert<mlir::scf::SCFDialect>();
+  registry.insert<mlir::LLVM::LLVMDialect>();
+  mlir::npc::registerAllDialects(registry);
+
+  return asMainReturnCode(MlirOptMain(argc, argv, "Numpy Compiler Opt Tool", registry));
+}
